32b1e9876c8b1dbb7bae21fe8623a99c77ebfb08
Kene Nduka <ndukasquad@gmail.com>
2024-06-04 10:36:33 +0100

    Contact Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Project from './components/Project'
+import ContactForm from './components/ContactForm'
 import './App.css'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Hero />
       <Project />
+      <ContactForm />
     </div>
   )
 }
